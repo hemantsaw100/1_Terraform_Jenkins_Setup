fd60d7c514c0d588bcfce3359e4b43c1b7590f84
hemant <sawhemant41@gmail.com>
2024-08-04 17:30:51 +0530

    Updated LB health check path

--- load-balancer-target-group/main.tf
+++ load-balancer-target-group/main.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "lb_target_group" {
     vpc_id      = var.vpc_id
 
     health_check{
-        path = "/login"
+        path = "/health"
         port = 8080
         protocol = "HTTP"
         timeout = 5
